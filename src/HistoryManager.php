d256a3547da67dc037ab8f238b81522bf29720a2
remontees <remontees@free.fr>
2013-06-13 21:12:51 +0200

    add methods
    actions about history and lists

--- src/HistoryManager.php
+++ src/HistoryManager.php
@@ -27,4 +27,28 @@ abstract class HistoryManager
 	 * @return int
 	 */
 	abstract public function countUser($user_id);
+	
+	/**
+	 * Méthode permettant de supprimer une entrée d'historique.
+	 * @param $id_history int L'identifiant de l'historique à supprimer
+	 * @return bool
+	 */
+	abstract public function delete($id);
+	
+	/**
+	 * Méthode retournant une liste d'historique précise.
+	 * @param $debut int La première entrée à sélectionner
+	 * @param $fin int La dernière entrée à sélectionner
+	 * @param $user_id int L'identifiant de l'utilisateur concerné par la requête
+	 * @return array La liste des entrées d'historiques. Chaque entrée du tableau est un objet History.
+	 */
+	abstract public function getList($debut = -1, $fin = -1, $user_id);
+	
+	/**
+	 * Méthode retournant une liste contenant les dernières entrées d'historiques que l'on veut.
+	 * @param $user_id int L'identifiant de l'utilisateur concerné
+	 * @param $nb_history int Le nombre d'entrées d'historiques que l'on veut
+	 * @return array La liste des dernières entrées d'historique du membre. Chaque entrée du tableau est un objet History.
+	 */
+	abstract public function getListLast($user_id, $nb_history);
 }
